5e8d131495b706990241c92b3e4673b29b9d1eba
Thomas Schmieck <mail@schmieck.de>
2021-10-31 15:26:31 +0100

    Add highlight switch and disable switch to card component

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,4 +1,9 @@
-<div class="card-component">
+<script lang="ts">
+    export let disabled: boolean = false;
+    export let highlight: boolean = false;
+</script>
+
+<div class="card-component" class:highlight class:disabled>
     <slot />
 </div>
 
@@ -15,8 +20,14 @@
         padding: 20px;
         font-size: 14px;
 
+        &.highlight,
         &:hover {
             background-color: functions.getHoverColor(colors.$primary);
+        }
+
+        &.disabled {
+            pointer-events: none;
+            opacity: 0.4;
         }
     }
 </style>
